62c3e144db0f000c7bb7cbd948a0389117eacf75
Amirul Naim <amirul431@gmail.com>
2026-07-28 13:34:17 +0800

    Remove OYA demo music player

--- showcase-app/app/oya/layout.tsx
+++ showcase-app/app/oya/layout.tsx
@@ -1,10 +1,3 @@
-import { OyaMediaPlayer } from '@/components/oya/OyaMediaPlayer';
-
 export default function OyaLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <OyaMediaPlayer />
-      {children}
-    </>
-  );
+  return children;
 }
